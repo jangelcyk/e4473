9c85aa9ee3cb7e4209e4ebfbe08dc63a21cc34fe
Jeff Angelcyk <jangelcyk@me.com>
2015-11-06 00:18:46 -0600

    validation additions and general cleanup

--- js/f4473validation.js
+++ js/f4473validation.js
@@ -19,16 +19,41 @@ $().ready(function() {
                 form_cust_county: "required",
                 form_cust_state: "required",
                 form_cust_zip: "required",
+//                form_cust_place_of_birth: "required",
+//                form_cust_state_of_birth: "required",
+//                form_cust_foreign_country: "required",
                 form_cust_height_ft: "required",
                 form_cust_height_in: "required",
                 form_cust_weight: "required",
                 form_cust_sex: "required",
+                birthdate: "required",
+//                form_cust_ssn: "requrired",
+//                form_cust_upin: "required", 
+                form_cust_ethnicity: "required",
+                form_cust_race: "required",
+                form_cust_question_11a: "required",
+                form_cust_question_11b: "required",
+                form_cust_question_11c: "required",
+                form_cust_question_11d: "required",
+                form_cust_question_11e: "required",
+                form_cust_question_11f: "required",
+                form_cust_question_11g: "required",
+                form_cust_question_11h: "required",
+                form_cust_question_11i: "required",
+                form_cust_question_11j: "required",
+                form_cust_question_11k: "required",
+                form_cust_question_11l: "required",
+                form_cust_question_12: "required",
+//                form_cust_state_residence: "required",
+                form_cust_citizenship: "required",
+                form_cust_alien_num: "required",
                 form_cust_state_residence: "required"
             },
             messages: {
                 form_cust_middle_name: "This field is required. NMN if no middle name."
             }
         });
+
         if (form.valid() == true){
             if ($('#page1').is(":visible")){
                 current_fs = $('#page1');
@@ -80,7 +105,29 @@ $().ready(function() {
             input.removeClass("valid").addClass("invalid");
         }
     });
+    
+    $('#form_cust_citizenship_us').change( function() {
+        if ($(this).is(":checked")) {
+            $('#form_cust_alien_num').val('');
+            $('#question15').hide();
+        }
+        else {
+            $('#question15').show();
+        }
+        
+    });
 
+    $('#form_cust_citizenship_other').change( function() {
+        if ($(this).is(":checked")) {
+            $('#question14b').show();
+        }
+        else {
+            $('#question14b').hide();
+            $('#form_cust_citizenship_other_desc').val('');
+
+        }
+    });
+    
     
 });
 
