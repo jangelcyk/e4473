16cb28e87bd85ac2d2a34a926c9fa4f8d1cd4524
Jeff Angelcyk <jangelcyk@me.com>
2015-11-10 15:37:01 -0600

    Added login form

--- js/f4473validation.js
+++ js/f4473validation.js
@@ -32,6 +32,14 @@ $().ready(function() {
         form.validate({
             
             rules: {
+                form_cust_num: {
+                    required: true,
+                    minlength: 6
+                },
+                form_cust_pin: {
+                    required: true,
+                    minlength: 3
+                },
                 form_cust_last_name: {
                     required: true,
                     minlength: 2,
@@ -160,44 +168,50 @@ $().ready(function() {
             
             // Verify customer is content with their answers before proceeding.
 
-            var verified = confirm("Are you sure you have answered the questions correctly");
-            
-            if (verified) {
-            
-                if ($('#page1').is(":visible")){
-                    current_fs = $('#page1');
-                    next_fs = $('#page2');
-                }
-                else if ($('#page2').is(":visible")){
-                    current_fs = $('#page2');
-                    next_fs = $('#page3');
-                }
-                else if ($('#page3').is(":visible")){
-                    current_fs = $('#page3');
-                    next_fs = $('#page4');
-                }
-                else if ($('#page4').is(":visible")){
-                    current_fs = $('#page4');
-                    next_fs = $('#page5');
-                }
-                else if ($('#page5').is(":visible")){
-                    current_fs = $('#page5');
-                    next_fs = $('#page6');
-                }
-                else if ($('#page6').is(":visible")){
-                    current_fs = $('#page6');
-                    next_fs = $('#page7');
-                    $('#formsubmit').show();
-                }
-                else if ($('#page7').is(":visible")){
-                    current_fs = $('#page7');
-                    next_fs = $('#page7');
-                    $('#formsubmit').show();
-                }
-
-                next_fs.show(); 
-                current_fs.hide();
+            if ($('#loginpage').is(":visible")){
+                current_fs = $('#loginpage');
+                next_fs = $('#page1');
             }
+            else {
+                
+                var verified = confirm("Are you sure you have answered the questions correctly");
+                
+                if (verified) {
+
+                    if ($('#page1').is(":visible")){
+                        current_fs = $('#page1');
+                        next_fs = $('#page2');
+                    }
+                    else if ($('#page2').is(":visible")){
+                        current_fs = $('#page2');
+                        next_fs = $('#page3');
+                    }
+                    else if ($('#page3').is(":visible")){
+                        current_fs = $('#page3');
+                        next_fs = $('#page4');
+                    }
+                    else if ($('#page4').is(":visible")){
+                        current_fs = $('#page4');
+                        next_fs = $('#page5');
+                    }
+                    else if ($('#page5').is(":visible")){
+                        current_fs = $('#page5');
+                        next_fs = $('#page6');
+                    }
+                    else if ($('#page6').is(":visible")){
+                        current_fs = $('#page6');
+                        next_fs = $('#page7');
+                        $('#formsubmit').show();
+                    }
+                    else if ($('#page7').is(":visible")){
+                        current_fs = $('#page7');
+                        next_fs = $('#page7');
+                        $('#formsubmit').show();
+                    }
+                }
+            }
+            next_fs.show(); 
+            current_fs.hide();
         }
     });            
     
@@ -213,6 +227,17 @@ $().ready(function() {
             input.removeClass("validx").addClass("invalidx");
         }
     });
+    
+    $('#form_cust_num').on('input', function() {
+        var input = $( this );
+        input.val( input.val().replace(/[^0-9]/g, '' ) );
+    });
+
+    $('#form_cust_pin').on('input', function() {
+        var input = $( this );
+        input.val( input.val().replace(/[^0-9]/g, '' ) );
+    });
+
     
     $('#form_cust_height_in').on('input', function() {
         var input = $( this );
